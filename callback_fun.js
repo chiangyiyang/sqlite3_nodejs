24d017bdb56135bc087c7aea01b0dabe3e928ef8
Yiyang Chiang <chiangyiyang@gmail.com>
2021-04-19 14:22:57 +0800

    13. 將Callback函數抽離

---
+++ callback_fun.js
@@ -0,0 +1,40 @@
+//載入Sqlite3
+const sqlite3 = require('sqlite3').verbose();
+
+//將回呼(Callback)函數分離成獨立的函數
+const callback = (msg) => {
+  return (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log(msg);
+  }
+};
+
+//開啟資料庫並連線
+let db = new sqlite3.Database('./sample.db',
+  callback('Connected to the database.'));  //使用新的回乎函數
+
+let records = [
+  { name: 'John', email: 'john@gmail.com' },
+  { name: 'Mary', email: 'mary@gmail.com' },
+  { name: 'Tom', email: 'tom@gmail.com' },
+]
+
+db.serialize(() => {
+  //建立資料表
+  db.run('CREATE TABLE IF NOT EXISTS users (name text, email text)',
+    callback('Table is created.')); //使用新的回乎函數
+
+  //插入多筆資料
+  let sql = `INSERT INTO users (name, email) VALUES ` + records.map(r => '(?, ?)').join(', ');
+  let data = records.flatMap(r => Object.values(r));
+
+  db.run(sql, data,
+    callback(`${records.length} records are inserted.`)); //使用新的回乎函數
+
+});
+
+//關閉資料庫
+db.close(callback('Close the database connection.')); //使用新的回乎函數
