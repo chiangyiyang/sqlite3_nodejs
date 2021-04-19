dfb41e1e1d75cc57514d08879451b8b0438d59b5
Yiyang <chiangyiyang@gmail.com>
2021-04-19 10:10:18 +0800

    7. 建立資料表

--- README.md
+++ README.md
@@ -83,3 +83,26 @@ db.close((err) => {
   console.log('Close the database connection.');
 });
 ```
+
+## 7. 建立資料表
+```js
+//載入Sqlite3
+...
+//開啟資料庫並連線
+...
+
+
+//建立資料表
+db.run('CREATE TABLE my_table(field1 text, field2 text)',
+  (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log('Table is created.');
+  });
+
+
+//關閉資料庫
+...
+```
---
+++ create_table.js
@@ -0,0 +1,35 @@
+//載入Sqlite3
+const sqlite3 = require('sqlite3').verbose();
+
+
+//開啟資料庫並連線
+let db = new sqlite3.Database('./my_database.db',
+  (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+
+    }
+    console.log('Connected to the database.');
+  });
+
+
+//建立資料表
+db.run('CREATE TABLE my_table(field1 text, field2 text)',
+  (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log('Table is created.');
+  });
+
+
+//關閉資料庫
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('Close the database connection.');
+});
